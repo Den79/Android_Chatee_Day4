7192facadc7b8fdb9354d505c364fef5527fe03b
Denis Maltev <A01171996@ad.bcit.ca>
2020-02-20 13:00:05 -0800

    Refactor Form as Login AND Registration

--- app/src/main/java/com/example/chateeinclass/MainActivity.kt
+++ app/src/main/java/com/example/chateeinclass/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.chateeinclass
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.activity_auth.*
+//import kotlinx.android.synthetic.main.activity_auth.login_button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,14 +16,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // write a message to Database
-        val database = FirebaseDatabase.getInstance();
-        val myRef = database.getReference("message");
-        myRef.setValue("Hello World!");
-
-        // TYEST
         login_button.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(AuthActivity.newIntent(AuthMode.Login, this))
+        }
+
+        register_button.setOnClickListener {
+            startActivity(AuthActivity.newIntent(AuthMode.Register, this))
         }
     }
+}
+
+sealed class AuthMode() : Parcelable {
+    @Parcelize
+    object Login : AuthMode()
+
+    @Parcelize
+    object Register : AuthMode()
 }
