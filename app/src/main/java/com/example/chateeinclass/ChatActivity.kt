29c300c46868f7f6072447dd8b853d7d25c74be4
Denis Maltev <A01171996@ad.bcit.ca>
2020-02-20 15:38:07 -0800

    Bug fix REMOVED

--- app/src/main/java/com/example/chateeinclass/ChatActivity.kt
+++ app/src/main/java/com/example/chateeinclass/ChatActivity.kt
@@ -9,8 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.item_message.view.*
 
@@ -29,22 +28,22 @@ class ChatActivity: AppCompatActivity() {
 
         message_list.adapter = MessagesAdapter(messages, this)
     }
+}
 
-    private class MessagesAdapter(private val messages: List<Message>, val context: Context): RecyclerView.Adapter<MessagesViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessagesViewHolder {
-            return MessagesViewHolder(LayoutInflater.from(context).inflate(R.layout.item_message, parent, false))
-        }
+private class MessagesAdapter(private val messages: List<Message>, val context: Context): RecyclerView.Adapter<MessagesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessagesViewHolder {
+        return MessagesViewHolder(LayoutInflater.from(context).inflate(R.layout.item_message, parent, false))
+    }
 
-        override fun getItemCount(): Int {
-            return messages.count()
-        }
+    override fun getItemCount(): Int {
+        return messages.count()
+    }
 
-        override fun onBindViewHolder(holder: MessagesViewHolder, position: Int) {
-            val message = messages[position]
+    override fun onBindViewHolder(holder: MessagesViewHolder, position: Int) {
+        val message = messages[position]
 
-            holder.itemView.sender_label.text = message.sender
-            holder.itemView.message_body_label.text = message.messageBody
-        }
+        holder.itemView.sender_label.text = message.sender
+        holder.itemView.message_body_label.text = message.messageBody
     }
 }
 
