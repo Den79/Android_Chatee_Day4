9d42f79905179fd8cc4de49f7f2f9f862efb0820
Denis Maltev <A01171996@ad.bcit.ca>
2020-02-20 09:43:49 -0800

    Connection to Firebase(Google) and writting first message

--- app/src/main/java/com/example/chateeinclass/MainActivity.kt
+++ app/src/main/java/com/example/chateeinclass/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.chateeinclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // write a message to Database
+        val database = FirebaseDatabase.getInstance();
+        val myRef = database.getReference("message");
+        myRef.setValue("Hello World!");
     }
 }
